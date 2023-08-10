9fea1c89af8a81c5cda7f0d53476cf070a4f5c6a
Deepthi-hp-rao <deepthihprao@gmail.com>
2023-08-10 13:14:37 -0700

    changed python file

--- app.py
+++ app.py
@@ -1,8 +1,1 @@
-def greet(name):
-    return f"Hello, {name}!"
-
-if __name__ == "__main__":
-    user_name = input("Enter your name: ")
-    message = greet(user_name)
-    print(message)
-
+print("Hello, world!")
